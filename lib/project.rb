d9bbef179f7c08d782a2c0b00da429d3dc58fd91
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-24 01:34:41 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Project
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def backers
+    @backers
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.project = self
+  end
+end
